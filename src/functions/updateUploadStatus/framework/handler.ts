3c90c6a388a26ac4d9fc64cd7c9c5c4603e75458
roberthall12 <roberth@kainos.com>
2019-06-07 10:10:40 +0100

    TSLint errors

--- src/functions/updateUploadStatus/framework/handler.ts
+++ src/functions/updateUploadStatus/framework/handler.ts
@@ -1,9 +1,8 @@
-import { APIGatewayProxyEvent, Context } from "aws-lambda";
+import { APIGatewayProxyEvent, Context } from 'aws-lambda';
 import Response from '../../../common/application/api/Response';
-import createResponse from "../../../common/application/utils/createResponse";
-import { HttpStatus } from "../../../common/application/api/HttpStatus";
-
+import createResponse from '../../../common/application/utils/createResponse';
+import { HttpStatus } from '../../../common/application/api/HttpStatus';
 
 export async function handler(event: APIGatewayProxyEvent, fnCtx: Context): Promise<Response> {
-    return createResponse({}, HttpStatus.CREATED);
+  return createResponse({}, HttpStatus.CREATED);
 }

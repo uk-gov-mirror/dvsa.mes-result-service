858faed2017370225052689a0fe0ab854538ecb8
Ross Faulds <ross.faulds@bjss.com>
2019-07-03 09:46:02 +0100

    add dummy handler to unblock devops to create pipeline

---
+++ src/functions/retryProcessing/framework/handler.ts
@@ -0,0 +1,8 @@
+import { APIGatewayEvent, Context } from 'aws-lambda';
+import createResponse from '../../../common/application/utils/createResponse';
+import { HttpStatus } from '../../../common/application/api/HttpStatus';
+import Response from '../../../common/application/api/Response';
+
+export async function handler(event: APIGatewayEvent, fnCtx: Context): Promise<Response> {
+  return createResponse({}, HttpStatus.OK);
+}

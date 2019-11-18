ab481e76df6daf971551c5198bafe6dc63d15c40
Robert Stepnowski <1335220+rstepnowski@users.noreply.github.com>
2019-11-18 11:04:29 +0000

    MES-4164 Modified the query of getNextBatch to handle multiple same appRefs te… (#104)
    * Modified the query of getNextBatch to handle multiple same appRefs tests conducted by different examiners
    
    * fixed the query - additional AND at the beginning of WHERE

--- src/functions/getNextUploadBatch/framework/database/query-builder.ts
+++ src/functions/getNextUploadBatch/framework/database/query-builder.ts
@@ -9,9 +9,13 @@ import * as mysql from 'mysql2';
 export const buildTarsNextBatchQuery = (batchSize: number, interfaceType: string): string => {
   const template = `
   SELECT TEST_RESULT.test_result
-  FROM TEST_RESULT JOIN UPLOAD_QUEUE
-  WHERE TEST_RESULT.application_reference = UPLOAD_QUEUE.application_reference
-  AND UPLOAD_QUEUE.interface = (SELECT id FROM INTERFACE_TYPE WHERE interface_type_name = ?)
+  FROM TEST_RESULT
+  JOIN UPLOAD_QUEUE ON
+    TEST_RESULT.application_reference = UPLOAD_QUEUE.application_reference
+    AND
+    TEST_RESULT.staff_number = UPLOAD_QUEUE.staff_number
+  WHERE
+  UPLOAD_QUEUE.interface = (SELECT id FROM INTERFACE_TYPE WHERE interface_type_name = ?)
   AND UPLOAD_QUEUE.upload_status = (SELECT id FROM PROCESSING_STATUS WHERE processing_status_name = 'PROCESSING')
   ORDER BY UPLOAD_QUEUE.timestamp ASC
   LIMIT ?;

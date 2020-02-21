9841befc04f40e3615019e3324d647edf7a2ac6c
Tom Billington <Cavalier07@users.noreply.github.com>
2020-02-21 16:23:22 +0000

    MES-4924 - Get Upload Batch Improved Logging [3.0.5] (#128)
    * Updated Log
    
    * Now return an error to upstream lambdas
    
    * Code review comments

--- src/functions/getNextUploadBatch/application/next-update-batch-service.ts
+++ src/functions/getNextUploadBatch/application/next-update-batch-service.ts
@@ -3,11 +3,5 @@ import { TestResultRecord } from '../../../common/domain/test-results';
 
 export const getNextUploadBatch = async (batchSize: number, interfaceType: string):
 Promise<TestResultRecord[]> => {
-  let batch;
-  try {
-    batch = await new BatchRepository().getUploadQueueData(batchSize, interfaceType);
-  } catch (err) {
-    console.error(`An error occured when attempting to retrieve a batch: ${err}`);
-  }
-  return batch;
+  return await new BatchRepository().getUploadQueueData(batchSize, interfaceType);
 };

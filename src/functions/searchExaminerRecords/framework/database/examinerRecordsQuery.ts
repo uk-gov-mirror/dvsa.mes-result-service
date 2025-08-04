76dd4b6095a750cd76b9337c61276706a8f91c39
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2025-08-04 17:24:36 +0100

    Edited examiner records backend query to not return extended tests

--- src/functions/searchExaminerRecords/framework/database/examinerRecordsQuery.ts
+++ src/functions/searchExaminerRecords/framework/database/examinerRecordsQuery.ts
@@ -1,24 +1,24 @@
 export const examinerRecordsQuery = `SELECT
-    application_reference AS 'appRef',
-    category AS 'testCategory',
-    activity_code AS 'activityCode',
+    application_reference AS appRef,
+    category AS testCategory,
+    activity_code AS activityCode,
     JSON_OBJECT(
         'centreId', tc_id,
         'costCode', tc_cc,
         'centreName', JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.journalData.testCentre.centreName'))
     ) AS testCentre,
     test_date AS 'startDate',
-    CAST(JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.testSummary.routeNumber')) as SIGNED) AS 'routeNumber',
-    JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.testData.controlledStop.selected')) AS 'controlledStop',
-    JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.journalData.testSlotAttributes.extendedTest')) AS 'extendedTest',
+    CAST(JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.testSummary.routeNumber')) as SIGNED) AS routeNumber,
+    JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.testData.controlledStop.selected')) AS controlledStop,
+    JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.journalData.testSlotAttributes.extendedTest')) AS extendedTest,
     CASE
-        WHEN JSON_CONTAINS_PATH(test_result, 'one', '$.journalData.testSlotAttributes.specialNeedsExtendedTest') 
+        WHEN JSON_CONTAINS_PATH(test_result, 'one', '$.journalData.testSlotAttributes.specialNeedsExtendedTest')
             THEN JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.journalData.testSlotAttributes.specialNeedsExtendedTest'))
         END AS 'specialNeedsExtendedTest',
-    JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.testSummary.independentDriving')) AS 'independentDriving',
-    JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.testSummary.circuit')) AS 'circuit',
-    JSON_EXTRACT(test_result, '$.testData.safetyAndBalanceQuestions.safetyQuestions') AS 'safetyQuestions',
-    JSON_EXTRACT(test_result, '$.testData.safetyAndBalanceQuestions.balanceQuestions') AS 'balanceQuestions',
+    JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.testSummary.independentDriving')) AS independentDriving,
+    JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.testSummary.circuit')) AS circuit,
+    JSON_EXTRACT(test_result, '$.testData.safetyAndBalanceQuestions.safetyQuestions') AS safetyQuestions,
+    JSON_EXTRACT(test_result, '$.testData.safetyAndBalanceQuestions.balanceQuestions') AS balanceQuestions,
     CASE
         WHEN JSON_CONTAINS_PATH(test_result, 'one', '$.testData.manoeuvres') THEN
             CASE
@@ -51,6 +51,8 @@ FROM TEST_RESULT
 WHERE test_date >= ? AND test_date <= ? 
   AND staff_number = ?
   AND activity_code in (1, 2, 3, 4, 5)
-  AND JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.journalData.testSlotAttributes.extendedTest')) != true
+  AND JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.journalData.testSlotAttributes.extendedTest')) != 'true'
+  AND !(JSON_CONTAINS_PATH(test_result, 'one', '$.journalData.testSlotAttributes.specialNeedsExtendedTest')
+    AND JSON_UNQUOTE(JSON_EXTRACT(test_result, '$.journalData.testSlotAttributes.specialNeedsExtendedTest')) = 'true')
 ORDER BY test_date DESC;
 `;

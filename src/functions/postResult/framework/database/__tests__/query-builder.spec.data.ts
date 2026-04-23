41265fc65a1a33303e5347eded61b1ee51975e43
Andrew Setterfield <48550267+andrewsetterfield@users.noreply.github.com>
2026-04-23 11:50:35 +0100

    feat: set a default testCentreId for dsp records (#378)
    * feat: set a default testCentreId for dsp records
    
    * feat: update DSP TC_ID default to use 0

--- src/functions/postResult/framework/database/__tests__/query-builder.spec.data.ts
+++ src/functions/postResult/framework/database/__tests__/query-builder.spec.data.ts
@@ -103,7 +103,7 @@ export const dummyTestResultDSP: TestResultSchemasUnion = {
       staffNumber: '999',
     },
     testCentre: {
-      centreId: 54321,
+      centreId: 0,
       costCode: 'EXTC1',
     },
     testSlotAttributes: {
--- src/functions/postResult/framework/database/query-builder.ts
+++ src/functions/postResult/framework/database/query-builder.ts
@@ -43,7 +43,8 @@ export const buildTestResultInsert = (
     const {staffNumber} = journalData.examiner;
     const testResult = JSON.stringify(test);
     const testDate = new Date(journalData.testSlotAttributes.start);
-    const testCentreId = journalData.testCentre.centreId;
+    // set default test centre id to 0 for DSP tests
+    const testCentreId = journalData?.applicationReference?.bookingReference ? 0 : journalData.testCentre.centreId;
     const testCentreCostCode = journalData.testCentre.costCode;
     const {driverNumber} = journalData.candidate;
     const driverSurname = journalData.candidate.candidateName.lastName;
